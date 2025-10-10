b3a40ab8d59d4b71fa1dff5ac396d9f96b1e1630
[dyad] <aminmaster@equilibria.ngo>
2025-10-09 23:34:23 -0500

    [dyad] Fixing Geist font subsets error - wrote 1 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,10 @@ import "./globals.css";
 
 const geistSans = GeistSans({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
 });
 
 const geistMono = GeistMono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 const queryClient = new QueryClient();
